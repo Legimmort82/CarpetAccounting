9278fde69e6500eb7377c1e4e21c9a26e6a20d2b
Yasin-A10 <yasinzizi8383@gmail.com>
2024-10-16 02:36:21 +0330

    add PWA

--- carpet-accounting/next.config.mjs
+++ carpet-accounting/next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true, 
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
+  ...pwaConfig,
   reactStrictMode: true,
 };
 
