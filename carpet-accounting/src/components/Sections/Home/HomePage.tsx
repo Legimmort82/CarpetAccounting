920a35b38d3dd9a5f159d87f2c2d1164e3583dab
Legimmort82 <legendlegendery1212@gmail.com>
2024-10-16 10:42:31 +0330

    homePage img

--- carpet-accounting/src/components/Sections/Home/HomePage.tsx
+++ carpet-accounting/src/components/Sections/Home/HomePage.tsx
@@ -1,11 +1,13 @@
 import Layout from "@/components/Layout/Layout"
+import carpetImg from "@/assets/carpet.jpg"
+import Image from "next/image"
 
 function HomePage() {
   return (
     <>
       <Layout>
-        <section className="h-screen bg-white w-full overflow-auto">
-          <h1 className="text-3xl font-bold">خانه</h1>
+        <section className="h-screen bg-white w-full overflow-auto flex justify-center items-center">
+          <Image src={carpetImg} alt="carpet"/>
         </section>
       </Layout>
     </>
