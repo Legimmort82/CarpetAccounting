06b91068dcf862e959646c0d6b8e94eb270e326a
Legimmort82 <legendlegendery1212@gmail.com>
2024-10-16 10:21:11 +0330

    add font

--- carpet-accounting/src/pages/_app.tsx
+++ carpet-accounting/src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
+import localFont from "next/font/local";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 const queryClient = new QueryClient();
+
+const myFont = localFont({ src: "../assets/fonts/Yekan.ttf" });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
+      <main className={myFont.className}>
+        <Component {...pageProps} />;
+      </main>
     </QueryClientProvider>
   );
 }
