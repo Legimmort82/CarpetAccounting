000e1a9770c02b57f5498f324c8b592dc503584a
Yasin-A10 <yasinzizi8383@gmail.com>
2024-10-18 13:46:36 +0330

    .

--- carpet-accounting/src/components/Layout/Layout.tsx
+++ carpet-accounting/src/components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ import workerLogo from "@/assets/sideBar/worker.svg";
 import salaryLogo from "@/assets/sideBar/salary.svg";
 import exitLogo from "@/assets/sideBar/exit.svg";
 import menu from "@/assets/sideBar/hamburger-menu.svg";
-import closeLogo from "@/assets/sideBar/close.svg"
+import closeLogo from "@/assets/sideBar/close.svg";
 import { LegacyRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
@@ -51,15 +51,19 @@ function Layout({ children }: props) {
         <AnimatePresence>
           <motion.div
             ref={SelectRef as LegacyRef<HTMLDivElement> | undefined}
-            initial={{x: "100vw"}}
-            animate={{x: openSidebar ? 0 : "100vw"}}
-            transition={{ duration: 0.4, ease: "easeOut"}}
-
-            className={` flex flex-col items-center bg-[#050A30] pr-[60px] z-96 h-screen fixed top-0 right-[-60px] min-w-[300px] overflow-y-auto overflow-x-hidden ${openSidebar ? "block" : "hidden"
-              } `}
+            initial={{ x: "100vw" }}
+            animate={{ x: openSidebar ? 0 : "100vw" }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            className={` flex flex-col items-center bg-[#050A30] pr-[60px] z-96 h-screen fixed top-0 right-[-60px] min-w-[300px] overflow-y-auto overflow-x-hidden ${
+              openSidebar ? "block" : "hidden"
+            } `}
           >
-
-            <Image onClick={openSidebarHandler} className="absolute top-2 left-2 cursor-pointer w-8 h-8 hover:scale-110 duration-200" src={closeLogo} alt="closeLogo" />
+            <Image
+              onClick={openSidebarHandler}
+              className="absolute top-2 left-2 cursor-pointer w-8 h-8 hover:scale-110 duration-200"
+              src={closeLogo}
+              alt="closeLogo"
+            />
 
             <Link href={"/"}>
               <Image
@@ -171,9 +175,13 @@ function Layout({ children }: props) {
           </motion.div>
         </AnimatePresence>
 
-
         <div className="flex-1">
-          <Image onClick={openSidebarHandler} className=' z-40 cursor-pointer fixed top-3 right-[15px] w-7 h-7 hover:scale-105 duration-200 sm:w-9 sm:h-9' src={menu} alt="menu" />
+          <Image
+            onClick={openSidebarHandler}
+            className=" z-40 cursor-pointer fixed top-3 right-[15px] w-7 h-7 hover:scale-105 duration-200 sm:w-9 sm:h-9"
+            src={menu}
+            alt="menu"
+          />
           {children}
         </div>
       </section>
