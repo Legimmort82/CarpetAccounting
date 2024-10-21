2042e4446ac5bba5438ce088f51f60bfd2c10b03
Yasin-A10 <yasinzizi8383@gmail.com>
2024-10-21 12:16:42 +0330

    disable PWA on dev mode

--- carpet-accounting/next.config.mjs
+++ carpet-accounting/next.config.mjs
@@ -9,7 +9,7 @@ const withPWA = createPWA({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
-  disable: false,
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = {
